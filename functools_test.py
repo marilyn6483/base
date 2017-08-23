2e934ae7b346f69e0744fd4f7e9899980e89edb4
982397045@qq.com <1280129660@qq.com>
2017-08-23 18:02:53 +0800

    add usage functool.wraps

--- functools_test.py
+++ functools_test.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 import functools
+from functools import wraps
 
 ''' functools模块用法示例'''
-
+#使用wraps装饰，或者使用functools.update_wrapper调用包装器函数实现重新绑定后__doc__,__name__的同步更新
 def decorator1(func):
+    @wraps(func)
     def wrapper(*args, **kwargs):
         print "call {}".format(func.__name__)
         res = func(*args, **kwargs)
@@ -23,8 +25,8 @@ def foo1():
 @decorator2
 def foo2():
     return "foo2"
+
 foo1()
 print foo1.__name__
-
 foo2()
 print foo2.__name__
